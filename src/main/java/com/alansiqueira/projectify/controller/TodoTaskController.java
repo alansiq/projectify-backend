e4161056943ecec8a16ecbe84d6f083f0561eaa3
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-03-20 21:36:11 -0300

    Added cors policy on tasks endpoint

--- src/main/java/com/alansiqueira/projectify/controller/TodoTaskController.java
+++ src/main/java/com/alansiqueira/projectify/controller/TodoTaskController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @AllArgsConstructor
 public class TodoTaskController {
