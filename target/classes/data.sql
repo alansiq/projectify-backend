61c902040204c4be8897d46f0de8e55b054940e3
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-04-24 21:32:40 -0300

    removed target

--- target/classes/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO PROJECT(title) VALUES ('Learn Java');
-INSERT INTO PROJECT(title) VALUES ('Learn Go');
--- INSERT INTO TODO_TASK(title, description, is_complete) VALUES('Create a fancy to-do app', 'it should be fancy af' , true);
--- INSERT INTO TODO_TASK(title, description, is_complete) VALUES('Create a fancy to-do app', 'it should be fancy af' , true);
--- INSERT INTO TODO_TASK(title, description, is_complete) VALUES('Create a fancy to-do app', 'it should be fancy af' , true);
--- INSERT INTO TODO_TASK(title, description, is_complete) VALUES('Create a fancy to-do app', 'it should be fancy af' , false);
--- INSERT INTO TODO_TASK(title, description, is_complete) VALUES('Create a fancy to-do app', 'it should be fancy af' , false);
--- INSERT INTO TODO_TASK(title, description, is_complete) VALUES('Create a fancy to-do app', 'it should be fancy af' , true);
--- INSERT INTO TODO_TASK(title, description, is_complete) VALUES('Create a fancy to-do app', 'it should be fancy af' , true);
