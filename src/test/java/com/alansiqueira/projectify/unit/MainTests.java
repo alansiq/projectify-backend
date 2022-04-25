28b7d62c00214a4f1877894497c1a6fe14455dc3
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-04-24 21:53:23 -0300

    Added a failing test to test github action integration

---
+++ src/test/java/com/alansiqueira/projectify/unit/MainTests.java
@@ -0,0 +1,16 @@
+package com.alansiqueira.projectify.unit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest
+public class MainTests {
+
+    @Test
+    public void shouldFail() {
+        fail();
+    }
+}
